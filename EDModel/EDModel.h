6ea73a65de4960e69d0d715a722663b99dcb80d3
Paul Sons <psons@psons@comcast.net>
2023-03-25 09:41:31 -0500

    Initial Commit

---
+++ EDModel/EDModel.h
@@ -0,0 +1,18 @@
+//
+//  EDModel.h
+//  EDModel
+//
+//  Created by Paul Sons on 3/25/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EDModel.
+FOUNDATION_EXPORT double EDModelVersionNumber;
+
+//! Project version string for EDModel.
+FOUNDATION_EXPORT const unsigned char EDModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EDModel/PublicHeader.h>
+
+
